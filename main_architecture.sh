6ab66dc57cc488f0f6535f33d08651bba9b5f0ab
Stein Acker <st1851ac-s@student.lu.se>
2021-03-10 11:08:27 +0100

    Got getopts to work in main_architecture.sh

--- main_architecture.sh
+++ main_architecture.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-while getopts ":i:p:"; do
+while getopts ":i:p:" opt; do
   case ${opt} in
     i ) fastq=$OPTARG ;;
-    p ) trim_primer=true; primers=$OPTARG
+    p ) trim_primer=true; primers=$OPTARG ;;
     \? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
   esac
 done
+shift $((OPTIND -1))
 
 if $trim_primer
 then
-  python3 scripts/trim_primer.py $fastq $primers ${fastq%.fastq}_aligned.fastq
+  python3 scripts/trim_primers.py $fastq $primers ${fastq%.fastq}_noprimers.fastq
+  fastq=${fastq%.fastq}_noprimers.fastq
 fi
