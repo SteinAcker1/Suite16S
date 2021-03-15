d3bc87ed71d4d731491cb0580aae14d1bcbf6fef
Stein Acker <st1851ac-s@student.lu.se>
2021-03-15 10:00:29 +0100

    Fixed minor bugs in the get SQL shell script

--- scripts/getBacteriaSQL.sh
+++ scripts/getBacteriaSQL.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat taxa/taxon.txt | cut -d$'\t' -f 11-16 | tr -d '[:punct:]' | sed 's/awaiting allocation//' | tr -d " " | grep 'Bacteria' | awk 'NF==6{print}{}'> taxa/bacteriaTaxa.tsv
+cat taxa/taxon.txt | cut -d$'\t' -f 11-16 | tr -d '[:punct:]' | tr -d " " | sed 's/awaitingallocation//g' |  grep 'Bacteria' | awk 'NF==6{print}{}'> taxa/bacteriaTaxa.tsv
 sqlite3 taxa/bacteriaTaxa.sqlite3 <<END_SQL
 .separator "\t"
 DROP TABLE taxa;
